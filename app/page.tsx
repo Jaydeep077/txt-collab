b75c2479d3835fbec929a717affb9e9668412052
Jaydeep <96289792+Jaydeep077@users.noreply.github.com>
2025-04-09 17:20:29 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -153,7 +153,7 @@ export default function Home() {
       <footer className="border-t py-6 md:py-0">
         <div className="container flex flex-col items-center justify-between gap-4 md:h-16 md:flex-row">
           <p className="text-center text-sm leading-loose text-gray-500 md:text-left">
-            @ TensorFlow07
+            @2025 (TensorFlow07)
           </p>
         </div>
       </footer>
